fdba8a0ee657f8e5b7e0e0e9a75089252c9eddf5
chaikivksyi <webrobs000@gmail.com>
2022-03-17 21:11:07 +0200

    add router

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,23 +2,18 @@
   <div>
     <Header />
     <div class="container">
-      <h1>test</h1>
-      <Products />
+      <router-view />
     </div>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
-import Products from "@/views/products";
-
-
 
 export default {
   name: 'App',
   components: {
-    Header,
-    Products
+    Header
   }
 }
 </script>
--- app/src/router/router.js
+++ app/src/router/router.js
@@ -1,3 +1,21 @@
-const router = require('vue-router');
+import { createWebHistory, createRouter } from "vue-router";
 
-module.exports = router;
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/home')
+    },
+    {
+        path: '/products',
+        name: 'Products',
+        component: () => import('../views/products')
+    }
+]
+
+// history: createWebHistory(process.env.BASE_URL),
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+export default router
