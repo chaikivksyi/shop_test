703193ae28b983df37f775bae2668917d95a98f2
chaikivksyi <webrobs000@gmail.com>
2022-03-19 21:29:49 +0200

    add Modal components

---
+++ app/src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+<!--  @click.self="$emit('closePopup')"-->
+  <div class="modal fade show d-flex align-items-center justify-content-center">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ modal_title }}</h5>
+          <button type="button" class="close" @click="$emit('closePopup')">
+            <span >&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <slot name="content"></slot>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" @click="$emit('successPopup')">{{ btn_text }}</button>
+          <button type="button" class="btn btn-secondary" @click="$emit('closePopup')">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app-modal",
+  props: {
+    modal_title: {
+      type: String,
+      default: 'Title'
+    },
+    btn_text: {
+      type: String,
+      default: 'Ok'
+    }
+  },
+  data() {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- app/src/views/products.vue
+++ app/src/views/products.vue
@@ -1,25 +1,34 @@
 <template>
   <div>
     <h1>Products</h1>
-    <form @submit.prevent method="post" class="m-3">
-      <div class="form-group mb-3">
-        <label for="title">Title</label>
-        <input type="text" v-model="product.title" class="form-control" name="title" id="title" placeholder="Enter title">
-      </div>
-      <div class="form-group mb-3">
-        <label for="price">Price</label>
-        <input type="number" v-model="product.price" class="form-control" name="price" id="price" placeholder="Enter price">
-      </div>
-      <div class="form-group mb-3">
-        <label for="category">Category</label>
-        <select v-model="product.category" id="category" name="category" class="form-select">
-          <option value="1" selected>1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-      </div>
-      <button @click="addProduct(product)" class="btn btn-primary w-25">Submit</button>
-    </form>
+    <button class="btn btn-success" @click="showModal = true">Add new product</button>
+    <Modal v-if="showModal"
+           @closePopup="showModal = false"
+           :title="'Add products product'"
+           :btn_text="'Add'"
+           @successPopup="addProduct"
+    >
+      <template v-slot:content>
+        <form @submit.prevent method="post" class="m-3">
+          <div class="form-group mb-3">
+            <label for="title">Title</label>
+            <input type="text" v-model="product.title" class="form-control" name="title" id="title" placeholder="Enter title">
+          </div>
+          <div class="form-group mb-3">
+            <label for="price">Price</label>
+            <input type="number" v-model="product.price" class="form-control" name="price" id="price" placeholder="Enter price">
+          </div>
+          <div class="form-group mb-3">
+            <label for="category">Category</label>
+            <select v-model="product.category" id="category" name="category" class="form-select">
+              <option value="1" selected>1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+          </div>
+        </form>
+      </template>
+    </Modal>
     <template v-if="!loader">
       <Table :obj="products"
              :type_fields="type_fields"
@@ -37,7 +46,7 @@
 import productsResources from "@/resources/products";
 import Loader from "@/components/Loader";
 import Table from "@/components/Table";
-
+import Modal from "@/components/Modal";
 
 export default {
   name: "app-products",
@@ -51,30 +60,30 @@ export default {
         img: 'default.jpg'
       },
       type_fields: [
-        {
-          head: 'Title',
-          label: 'title',
-        },
-        {
-          head: 'Price',
-          label: 'price',
-        },
-        {
-          head: 'Category',
-          label: 'category',
-        },
+        {head: 'Title', label: 'title',},
+        {head: 'Price', label: 'price',},
+        {head: 'Category', label: 'category',},
       ],
       loader: true,
+      showModal: false
     }
   },
   components: {
     Loader,
-    Table
+    Table,
+    Modal
   },
   methods: {
-    addProduct(product) {
-      productsResources.addProduct(product).then(response => {
+    addProduct() {
+      productsResources.addProduct(this.product).then(response => {
         this.products.push(response.data)
+        this.showModal = false
+        this.product = {
+            title: '',
+            price: 0,
+            category: 1,
+            img: 'default.jpg'
+        }
       }).catch(err => {
         console.log(err)
       });
@@ -97,7 +106,7 @@ export default {
           setTimeout(() => {
             this.products = response.data
             this.loader = false;
-          }, 2000)
+          }, 500)
 
         }).catch(error => {
       console.log(error)
