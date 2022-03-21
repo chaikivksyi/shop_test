1d843ec08a6923fba0ef0e855a00d10ebb443388
chaikivksyi <webrobs000@gmail.com>
2022-03-21 09:23:17 +0200

    add public project

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -10,7 +10,7 @@
         <li class="nav-item active">
           <router-link to="/" class="nav-link">Home</router-link>
         </li>
-        <li class="nav-item">
+        <li class="ndsfav-item">
           <router-link to="/products" class="nav-link">Products</router-link>
         </li>
         <li class="nav-item">
